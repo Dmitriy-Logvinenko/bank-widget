fbb80c3686a650199f302d175741e458b35364d4
Dmitriy Logvinenko <4iDmitriy@gmail.com>
2026-08-03 17:45:55 +0300

    init git

---
+++ src/masks.py
@@ -0,0 +1,24 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Возвращает скрытый номер карты."""
+    if card_number.isdigit() and len(card_number) == 16:
+        mask_number = card_number[:6] + "******" + card_number[-4:]
+        number_chunks = [mask_number[i : i + 4] for i in range(0, len(mask_number), 4)]
+        return " ".join(number_chunks)
+    else:
+        return "Номер карты должен содержать 16 цифр без пробелов."
+
+
+def get_mask_account(account_number: str) -> str:
+    """Возвращает скрытый номер счёта."""
+    if not account_number.isdigit():
+        return "Номер счёта должен содержать только цифры без пробелов."
+    elif len(account_number) < 4:
+        return "Номер счёта не может быть короче четырёх символов."
+    else:
+        mask_number = "**" + account_number[-4:]
+        return mask_number
+
+
+if __name__ == "__main__":
+    print(get_mask_card_number(input("Введите номер карты: ")))
+    print(get_mask_account(input("Введите номер счёта: ")))
---
+++ tests/test_masks.py
@@ -0,0 +1,5 @@
+import src.masks
+
+# Тесты функций скрытия номеров
+print(src.masks.get_mask_card_number(input("Введите номер карты: ")))
+print(src.masks.get_mask_account(input("Введите номер счёта: ")))
