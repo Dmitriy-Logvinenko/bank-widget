64a81788905342310d4243ae7bfcfdf235b61c9a
Dmitriy Logvinenko <4iDmitriy@gmail.com>
2026-08-05 10:28:19 +0300

    update func mask account

--- src/widget.py
+++ src/widget.py
@@ -4,11 +4,15 @@ import src.masks as masks
 def mask_account_card(user_number: str) -> str:
     """Обрабатывает информацию о картах и счетах"""
     new_list = user_number.split()
+
     for i in range(1):
-        if new_list[0] == 'Cчет':
+        if new_list[0] == 'Счет':
             new_list[1] = masks.get_mask_account(new_list[1])
+        elif new_list[1].isdigit():
+            new_list[1] = masks.get_mask_card_number(new_list[1])
+        elif new_list[2].isdigit():
+            new_list[2] = masks.get_mask_card_number(new_list[2])
+        else:
+            return 'Некоректно введены данные.'
 
     return ' '.join(new_list)
-
-
-print(mask_account_card('Cчет 765849673576389'))
