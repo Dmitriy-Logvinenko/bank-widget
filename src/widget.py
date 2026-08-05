2960f18461ab9e60f10435eb5937979d3c5ebe0b
Dmitriy Logvinenko <4iDmitriy@gmail.com>
2026-08-05 11:16:10 +0300

    add protection of uncorrect input

--- src/widget.py
+++ src/widget.py
@@ -5,14 +5,16 @@ def mask_account_card(user_number: str) -> str:
     """Обрабатывает информацию о картах и счетах"""
     new_list = user_number.split()
 
-    for i in range(1):
-        if new_list[0] == 'Счет':
-            new_list[1] = masks.get_mask_account(new_list[1])
-        elif new_list[1].isdigit():
-            new_list[1] = masks.get_mask_card_number(new_list[1])
-        elif new_list[2].isdigit():
-            new_list[2] = masks.get_mask_card_number(new_list[2])
-        else:
-            return 'Некоректно введены данные.'
-
+    if user_number:
+        for i in range(1):
+            if new_list[0] == 'Счет':
+                new_list[1] = masks.get_mask_account(new_list[1])
+            elif new_list[1].isdigit():
+                new_list[1] = masks.get_mask_card_number(new_list[1])
+            elif new_list[2].isdigit():
+                new_list[2] = masks.get_mask_card_number(new_list[2])
+            else:
+                return 'Некоректно введены данные.'
+    else:
+        return 'Вы ничего не ввели.'
     return ' '.join(new_list)
