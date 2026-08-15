b9c95b2eda88f2c631cf6a55f7ab7315857d6ad5
Dmitriy Logvinenko <4iDmitriy@gmail.com>
2026-08-15 10:57:51 +0300

    update docstrings

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,16 @@
-def filter_by_state(user_list: list[dict], state: str = 'EXECUTED') -> list[dict]:
-    """Принимает список словарей и ключ. Возвращает список словарей по ключу"""
+from typing import Any
+
+def filter_by_state(user_list: list[dict[str, Any]], state: str = 'EXECUTED') -> list[dict[str, Any]]:
+    """
+    Принимает список словарей с данными о банковских операциях и фильтрует их по указанному состоянию.
+
+    :param user_list: список словарей, содержащих данные о банковских операциях.
+    :type user_list: list[dict[str, Any]]
+    :param state: состояние, по которому фильтруются операции, по умолчанию 'EXECUTED'.
+    :type state: str
+    :return: список словарей, отфильтрованных по состоянию.
+    :rtype: list[dict[str, Any]]
+    """
     new_list = list()
 
     for item in user_list:
@@ -9,8 +20,16 @@ def filter_by_state(user_list: list[dict], state: str = 'EXECUTED') -> list[dict
     return new_list
 
 
-def sort_by_date(user_list: list[dict], sorter: bool = True) -> list[dict]:
-    """Принимает список словарей и параметр сортировки. Возвращает отсортированный список словарей."""
+def sort_by_date(user_list: list[dict[str, Any]], sorter: bool = True) -> list[dict[str, Any]]:
+    """
+    Принимает список словарей  данными о банковских операциях и фильтрует их по указанному направлению.
+    :param user_list: список словарей, содержащих данные о банковских операциях.
+    :type user_list: list[dict[str, Any]]
+    :param sorter: направление, по которому фильтруются операции, по умолчанию 'True'.
+    :type sorter: bool
+    :return: список словарей, отфильтрованных по направлению.
+    :rtype: list[dict[str, Any]]
+    """
     new_list = sorted(user_list, key=lambda k: k['date'], reverse=sorter)
 
     return new_list
