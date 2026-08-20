d1fb8d867dc93a3b50cacdcc389e1aaaabb45852
Dmitriy Logvinenko <4iDmitriy@gmail.com>
2026-08-20 13:16:37 +0300

    add tests for func get_mask_card_number

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,5 +1,19 @@
-import src.masks
+import pytest
+from src.masks import get_mask_card_number
 
-# Тесты функций скрытия номеров
-print(src.masks.get_mask_card_number(input("Введите номер карты: ")))
-print(src.masks.get_mask_account(input("Введите номер счёта: ")))
+
+@pytest.mark.parametrize("card_number, expected", [
+    ("7000792289606361", "7000 79** **** 6361"),
+    ("1596837868705199", "1596 83** **** 5199"),
+    ("6831982476737658", "6831 98** **** 7658")
+])
+def test_get_mask_card_number(card_number: str, expected: str):
+    assert get_mask_card_number(card_number) == expected
+
+
+def test_get_card_number_len(card_number: str):
+    assert get_mask_card_number(card_number + '1') == "Номер карты должен содержать 16 цифр без пробелов."
+
+
+def test_get_card_number_correct():
+    assert get_mask_card_number('Номер карты') == "Номер карты должен содержать 16 цифр без пробелов."
