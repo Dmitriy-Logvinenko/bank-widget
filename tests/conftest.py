acb50987b96a6b6a372e9c5076ab31e1af110b4f
Dmitriy Logvinenko <4iDmitriy@gmail.com>
2026-08-20 13:33:23 +0300

    add tests for func get_mask_account

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,8 @@ import pytest
 @pytest.fixture()
 def card_number():
     return "7000792289606361"
+
+
+@pytest.fixture()
+def account_number():
+    return "73654108430135874305"
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,5 +1,7 @@
 import pytest
-from src.masks import get_mask_card_number
+
+from src import masks
+from src.masks import get_mask_card_number, get_mask_account
 
 
 @pytest.mark.parametrize("card_number, expected", [
@@ -17,3 +19,20 @@ def test_get_card_number_len(card_number: str):
 
 def test_get_card_number_correct():
     assert get_mask_card_number('Номер карты') == "Номер карты должен содержать 16 цифр без пробелов."
+
+
+@pytest.mark.parametrize("account_number, expected", [
+    ("73654108430135874305", "**4305"),
+    ("64686473678894779589", "**9589"),
+    ("35383033474447895560", "**5560")
+])
+def test_get_mask_account(account_number: str, expected: str):
+    assert get_mask_account(account_number) == expected
+
+
+def test_get_mask_account_no_isdigit(account_number):
+    assert get_mask_account("Номер счёта") == "Номер счёта должен содержать только цифры без пробелов."
+
+
+def test_get_mask_account_len():
+    assert get_mask_account("345") == "Номер счёта не может быть короче четырёх символов."
