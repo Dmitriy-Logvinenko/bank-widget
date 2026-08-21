e93ea1662c2d2d919a394e09d0517433a59b61c2
Dmitriy Logvinenko <4iDmitriy@gmail.com>
2026-08-21 12:18:50 +0300

    update func for IndexError

--- src/widget.py
+++ src/widget.py
@@ -8,14 +8,17 @@ def mask_account_card(user_number: str) -> str:
 
     if user_number:
         for i in range(1):
-            if new_list[0] == "Счет":
-                new_list[1] = masks.get_mask_account(new_list[1])
-            elif new_list[1].isdigit():
-                new_list[1] = masks.get_mask_card_number(new_list[1])
-            elif new_list[2].isdigit():
-                new_list[2] = masks.get_mask_card_number(new_list[2])
-            else:
-                return "Некоректно введены данные."
+
+            try:
+                if new_list[0] == "Счет":
+                    new_list[1] = masks.get_mask_account(new_list[1])
+                elif new_list[1].isdigit():
+                    new_list[1] = masks.get_mask_card_number(new_list[1])
+                elif new_list[2].isdigit():
+                    new_list[2] = masks.get_mask_card_number(new_list[2])
+
+            except IndexError:
+                return "Некорректно введены данные."
     else:
         return "Вы ничего не ввели."
     return " ".join(new_list)
@@ -26,8 +29,3 @@ def get_date(user_date: str) -> str:
     new_date = user_date[:10].split("-")
     reversed_date = new_date[::-1]
     return ".".join(reversed_date)
-
-
-if __name__ == "__main__":
-    print(mask_account_card(input()))
-    print(get_date(input()))
