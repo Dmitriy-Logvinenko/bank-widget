67bcca9877d57290e5e006275587f13cde9c15f0
Dmitriy Logvinenko <4iDmitriy@gmail.com>
2026-08-26 18:28:54 +0300

    add docstrings

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,14 +1,14 @@
 from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 
 
-def test_filter_by_currency(transactions, currency):
+def test_filter_by_currency(transactions: list[dict], currency: str):
     generator = filter_by_currency(transactions, currency)
     assert next(generator) == transactions[0]
     assert next(generator) == transactions[1]
     assert next(generator) == transactions[3]
 
 
-def test_transaction_descriptions(transactions):
+def test_transaction_descriptions(transactions: list[dict]):
     generator = transaction_descriptions(transactions)
     assert next(generator) == "Перевод организации"
     assert next(generator) == "Перевод со счета на счет"
