2d3ba34bb2ec624e2911e77026473cc483fd6d71
Dmitriy Logvinenko <4iDmitriy@gmail.com>
2026-08-26 16:27:03 +0300

    add func transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -16,3 +16,16 @@ def filter_by_currency(transactions: list[dict], currency: str) -> Any:
         for transaction in transactions:
             if transaction["operationAmount"]["currency"]["code"] == currency:
                 yield transaction
+
+
+def transaction_descriptions(transactions: list[dict]) -> Any:
+    """
+    Принимает транзакции и возвращает их описание.
+    :param transactions: Список словарей, представляющих транзакции
+    :type transactions: list[dict]
+    :return: Описание транзакций
+    :rtype: Any
+    """
+    while True:
+        for transaction in transactions:
+            yield transaction["description"]
