94c8e9cb235e4e8be6f616ad6c5e0a3627c43b50
Dmitriy Logvinenko <4iDmitriy@gmail.com>
2026-08-26 17:24:57 +0300

    add test for func transaction_descriptions

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,8 +1,15 @@
-from typing import Any
-
-from src.generators import filter_by_currency
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 
 
 def test_filter_by_currency(transactions, currency):
     generator = filter_by_currency(transactions, currency)
     assert next(generator) == transactions[0]
+    assert next(generator) == transactions[1]
+    assert next(generator) == transactions[3]
+
+
+def test_transaction_descriptions(transactions):
+    generator = transaction_descriptions(transactions)
+    assert next(generator) == "Перевод организации"
+    assert next(generator) == "Перевод со счета на счет"
+    assert next(generator) == "Перевод со счета на счет"
