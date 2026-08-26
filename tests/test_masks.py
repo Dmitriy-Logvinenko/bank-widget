7302694cfabb4258e840f53b20855ce476563d44
Dmitriy Logvinenko <4iDmitriy@gmail.com>
2026-08-26 17:52:48 +0300

    correcting names

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -12,11 +12,11 @@ def test_get_mask_card_number(card_number: str, expected: str):
     assert get_mask_card_number(card_number) == expected
 
 
-def test_get_card_number_len(card_number: str):
+def test_get_mask_card_number_len(card_number: str):
     assert get_mask_card_number(card_number + '1') == "Номер карты должен содержать 16 цифр без пробелов."
 
 
-def test_get_card_number_correct():
+def test_get_mask_card_number_correct():
     assert get_mask_card_number('Номер карты') == "Номер карты должен содержать 16 цифр без пробелов."
 
 
