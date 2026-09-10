4811c36ba62f9d52f9969e488d7b18ad6c7fece0
Dmitriy Logvinenko <4iDmitriy@gmail.com>
2026-09-10 13:54:10 +0300

    add test for func financial_transaction_data

---
+++ tests/test_utils.py
@@ -0,0 +1,16 @@
+import json
+from unittest.mock import patch, Mock
+
+from src.utils import financial_transaction_data
+
+
+@patch('json.load')
+def test_financial_transaction_data(mock_get) -> None:
+    mock_get.return_value = []
+    assert financial_transaction_data('filename.json') == []
+
+
+def test_financial_transaction_data_with_mock():
+    mock_value = Mock(return_value=[])
+    json.load = mock_value
+    assert financial_transaction_data('filename.json') == []
