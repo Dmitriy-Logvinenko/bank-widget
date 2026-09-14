9b7b92a57ac2c7ea08b498141f0504175f3ba6f5
Dmitriy Logvinenko <4iDmitriy@gmail.com>
2026-09-14 19:25:50 +0300

    add logger for utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,12 @@
 import json
+import logging
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("logs/utils.log", "w", encoding='utf-8')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def financial_transaction_data(filename: str) -> list:
@@ -10,13 +18,17 @@ def financial_transaction_data(filename: str) -> list:
     :rtype: list
     """
     try:
+        logger.info("Выполняется чтение файла.")
         with open('data/' + filename, 'r', encoding='utf-8') as f:
             data = json.load(f)
 
             if not isinstance(data, list):
+                logger.info("Файл пуст.")
                 data = []
 
+            logger.info("Чтение файла завершено.")
             return data
 
-    except (json.JSONDecodeError, FileNotFoundError):
+    except (json.JSONDecodeError, FileNotFoundError) as ex:
+        logger.error(f"Произошла ошибка {ex}")
         return []
