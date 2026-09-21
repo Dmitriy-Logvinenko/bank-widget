377c440fdfad83971e58d856fbe0075d5209414c
Dmitriy Logvinenko <4iDmitriy@gmail.com>
2026-09-21 23:59:30 +0300

    add func process_bank_operations

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,15 @@
 import re
 
-from typing import Any
 
-
-def filter_by_state(user_list: list[dict[str, Any]], state: str = 'EXECUTED') -> list[dict[str, Any]]:
+def filter_by_state(user_list: list[dict], state: str = 'EXECUTED') -> list[dict]:
     """
     Принимает список словарей с данными о банковских операциях и фильтрует их по указанному состоянию.
     :param user_list: Данные о банковских операциях
-    :type user_list: list[dict[str, Any]]
+    :type user_list: list[dict]
     :param state: Состояние, по которому фильтруются операции, по умолчанию 'EXECUTED'
     :type state: str
     :return: список словарей, отфильтрованных по состоянию
-    :rtype: list[dict[str, Any]]
+    :rtype: list[dict]
     """
     new_list = list()
 
@@ -22,15 +20,15 @@ def filter_by_state(user_list: list[dict[str, Any]], state: str = 'EXECUTED') ->
     return new_list
 
 
-def sort_by_date(user_list: list[dict[str, Any]], is_sorted: bool = True) -> list[dict[str, Any]]:
+def sort_by_date(user_list: list[dict], is_sorted: bool = True) -> list[dict]:
     """
     Принимает список словарей данными о банковских операциях и фильтрует их по указанному направлению.
     :param user_list: Данные о банковских операциях
-    :type user_list: list[dict[str, Any]]
+    :type user_list: list[dict]
     :param is_sorted: направление, по которому фильтруются операции, по умолчанию 'True'
     :type is_sorted: bool
     :return: список словарей, отфильтрованных по направлению
-    :rtype: list[dict[str, Any]]
+    :rtype: list[dict]
     """
     new_list = sorted(user_list, key=lambda k: k['date'], reverse=is_sorted)
 
@@ -63,3 +61,36 @@ def process_bank_search(data: list[dict], search: str) -> list[dict]:
         print(e)
 
     return search_list
+
+
+def process_bank_operations(data: list[dict], categories: list) -> dict:
+    """
+    Подсчитывает общее количество операций по заданным категориям.
+    :param data: Данные о банковских операциях
+    :type data: list[dict]
+    :param categories: Категории операций
+    :type categories: list
+    :return: Операции по заданным категориям и их количество
+    :rtype: dict
+    """
+    operations = {}
+
+    try:
+        for category in categories:
+            # Список вхождений отдельной категории
+            category_list = []
+
+            for item in data:
+                if 'description' in item:
+                    # Проверка на наличие категории
+                    if re.search(category, item['description']):
+                        # Запись категории в список
+                        category_list.append(category)
+
+            # Подсчёт определённой категории
+            operations[category] = len(category_list)
+
+    except Exception as e:
+        print(e)
+
+    return operations
